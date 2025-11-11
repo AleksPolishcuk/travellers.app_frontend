ce9d1bc092c1165ff3bf6f907f85547e0771dc36
ta_imk <timofejzagirnij@gmail.com>
2025-11-12 00:19:09 +0200

    FixUser.ts

--- types/user.ts
+++ types/user.ts
@@ -1,29 +1,29 @@
 export interface User {
-    _id: string;
-    name: string;
-    email: string;
-    avatar?: string;
-    description?: string;
-    onboardingCompleted: boolean;
-    savedStories?: string[];
-  }
-  
-  export interface RegisterRequest {
-    name: string;
-    email: string;
-    password: string;
-  }
-  
-  export interface LoginRequest {
-    email: string;
-    password: string;
-  }
-  
-  export interface AuthResponse {
-    status: number;
-    message: string;
-    data: User;
-  }
+  _id: string;
+  name: string;
+  email: string;
+  avatar?: string;
+  description?: string;
+  onboardingCompleted: boolean;
+  savedStories?: string[];
+}
+
+export interface RegisterRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse {
+  status: number;
+  message: string;
+  data: User;
+}
 
 export interface Traveller {
   _id: string;
@@ -41,13 +41,13 @@ export interface TravellersResponseData {
   totalPages: number;
   hasNextPage: boolean;
   hasPreviousPage: boolean;
-}Ï
+}
 
-  type AuthStore = {
-    isAuthenticated: boolean;
-    user: User | null;
-    isLoading: boolean;
-    setUser: (user: User) => void;
-    clearUser: () => void;
-    setLoading: (loading: boolean) => void;
-  };
+type AuthStore = {
+  isAuthenticated: boolean;
+  user: User | null;
+  isLoading: boolean;
+  setUser: (user: User) => void;
+  clearUser: () => void;
+  setLoading: (loading: boolean) => void;
+};
