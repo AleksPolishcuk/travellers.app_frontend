81cf02e1747b36d2dfe2a85500b59efe22790946
v1tyka <racinvictor@gmail.com>
2025-11-12 19:39:01 +0100

    commit 1

--- app/auth/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from '@/app/components/AuthForm/AuthForm';
-
-export default function LoginPage() {
-  return (
-    <div className="auth-page">
-      <AuthForm initialTab="sign-in" />
-    </div>
-  );
-}
--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,10 +1,3 @@
-
-
 export default function Header() {
-  return (
-    <div>
-      Header
-  
-    </div>
-  );
+  return <div></div>;
 }
