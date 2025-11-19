ef52a55f4f980626c27c2e0f3ca10a70d019ba68
ta_imk <timofejzagirnij@gmail.com>
2025-11-20 01:01:45 +0200

    AddTravellerIdPage

---
+++ app/components/TravellerPage/TravellerStoryCard.tsx
@@ -0,0 +1,113 @@
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import styles from './TravellerCardStory.module.css';
+import { Story } from '@/types/story';
+import { useUser } from '@/lib/hooks/useUser';
+import { saveStory, removeSavedStory } from '@/lib/api/clientApi';
+
+interface StoryCardProps {
+  story: Story;
+}
+
+export function TravellerStoryCard({ story }: StoryCardProps) {
+  const router = useRouter();
+  const { user } = useUser();
+  const [articlesAmount, setArticlesAmount] = useState(
+    story.ownerId.articlesAmount,
+  );
+
+  const [isSaved, setIsSaved] = useState(story.isSaved || false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSaveClick = async () => {
+    if (!user) {
+      router.push('/auth/register');
+      return;
+    }
+
+    try {
+      setLoading(true);
+
+      if (isSaved) {
+        await removeSavedStory(story._id);
+        setArticlesAmount((prev) => prev - 1);
+      } else {
+        await saveStory(story._id);
+        setArticlesAmount((prev) => prev + 1);
+      }
+
+      setIsSaved(!isSaved);
+    } catch (err: any) {
+      console.error('Помилка збереження:', err);
+      alert(err.message || 'Помилка збереження');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className={styles.listItem}>
+      <div className={styles.storyImg}>
+        <img className={styles.listImg} src={story.img} alt={story.title} />
+      </div>
+      <div className={styles.storyContent}>
+        <div className={styles.content}>
+          <p className={styles.category}>{story.category?.name}</p>
+          <h3 className={styles.title}>{story.title}</h3>
+          <p className={styles.article}>
+            {story.article
+              ? story.article.substring(0, 90) + '...'
+              : 'Опис відсутній'}
+          </p>
+        </div>
+        <div className={styles.user}>
+          <div className={styles.imgCard}>
+            <img className={styles.userImg} src={story.ownerId.avatarUrl} />
+            <div className={styles.userInfo}>
+              <h4 className={styles.userName}>{story.ownerId.name}</h4>
+
+              <div className={styles.userContent}>
+                <span className={styles.listDate}>{story.date}</span>
+                <span className={styles.point}></span>
+
+                <div className={styles.rate}>
+                  <span className={styles.userCount}>{articlesAmount}</span>
+                  <svg viewBox="4 4 16 16" className={styles.userBookmark}>
+                    <use href="/icons.svg#icon-bookmark"></use>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.storyView}>
+          <Link href={`/stories/${story._id}`} passHref>
+            <button type="button" className={styles.storyButton}>
+              Переглянути статтю
+            </button>
+          </Link>
+          <button
+            type="button"
+            className={styles.saveButton}
+            onClick={handleSaveClick}
+            disabled={loading}
+          >
+            <svg width="24" height="24">
+              <use
+                className={styles.icon}
+                href={
+                  isSaved
+                    ? '/icons.svg#icon-bookmark-filled'
+                    : '/icons.svg#icon-bookmark'
+                }
+              ></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/components/TravellerPage/TravellersListSaved.tsx
@@ -0,0 +1,143 @@
+'use client';
+
+import { useState, useEffect, useRef } from 'react';
+import { getTravellerById } from '@/lib/api/travellerload';
+import styles from './TravellerCard.module.css';
+import { useScreenSize } from '@/lib/hooks/useScreenSize';
+import { TravellerStoryCard } from './TravellerStoryCard';
+import { Story } from '@/types/story';
+import Loader from '@/app/components/Loader/Loader';
+
+interface TravellerStoriesProps {
+  travellerId: string ;
+}
+
+const TravellerStories = ({ travellerId }: TravellerStoriesProps) => {
+  const [stories, setStories] = useState<Story[]>([]);
+  const [buffer, setBuffer] = useState<Story[]>([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isFetching, setIsFetching] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const loadMoreRef = useRef<HTMLButtonElement | null>(null);
+
+  const width = useScreenSize();
+  const perPage = width > 1024 ? 9 : width > 768 ? 8 : 9;
+
+  useEffect(() => {
+    const fetchStories = async () => {
+      if (!travellerId) return;
+      try {
+        setLoading(true);
+        const response = await getTravellerById(travellerId);
+        const list = response?.data?.stories ?? [];
+        const user = response?.data?.user;
+
+        if (Array.isArray(list)) {
+          const storiesWithOwner = list.map((story) => ({
+            ...story,
+            ownerId: user,
+          }));
+
+          setStories(storiesWithOwner.slice(0, perPage));
+          setBuffer(storiesWithOwner.slice(perPage));
+        } else {
+          console.warn('Unexpected response structure:', response);
+          setStories([]);
+        }
+      } catch (err) {
+        console.error('Error fetching stories:', err);
+        setError(
+          err instanceof Error ? err.message : 'Failed to fetch stories',
+        );
+        setStories([]);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchStories();
+  }, [travellerId, perPage]);
+
+  const renderFromBuffer = () => {
+    const toRender = buffer.slice(0, perPage);
+    setStories((prev) => [...prev, ...toRender]);
+    setBuffer((prev) => prev.slice(perPage));
+  };
+
+  const fetchMore = async () => {
+    const nextPage = currentPage + 1;
+    setCurrentPage(nextPage);
+    setIsFetching(true);
+
+    try {
+      const response = await getTravellerById(travellerId);
+      const list = response?.data?.stories ?? [];
+
+      setBuffer((prev) => [...prev, ...list]);
+
+      if (list.length < perPage) {
+        setHasMore(false);
+      }
+    } catch (err) {
+      console.error('Error loading more stories', err);
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
+  useEffect(() => {
+    if (buffer.length >= perPage && !isFetching) {
+      renderFromBuffer();
+    }
+  }, [buffer, isFetching]);
+
+  const handleLoadMore = () => {
+    loadMoreRef.current?.blur();
+    if (buffer.length >= perPage) {
+      renderFromBuffer();
+    } else if (!isFetching) {
+      fetchMore();
+    }
+  };
+
+  if (loading) return <Loader />;
+  if (error) return <div className={styles.error}>Помилка: {error}</div>;
+  if (!stories || stories.length === 0)
+    return <div className={styles.empty}>Історії відсутні</div>;
+
+  return (
+    <section className={styles.stories}>
+      <div className={styles.story}>
+        <ul className={styles.list}>
+          {Array.isArray(stories) &&
+            stories.map((story) => (
+              <li key={story._id}>
+                <TravellerStoryCard story={story} />
+              </li>
+            ))}
+        </ul>
+
+        <div className={styles.readBtn}>
+          {isFetching ? (
+            <Loader />
+          ) : hasMore ? (
+            <button
+              className={styles.readButton}
+              ref={loadMoreRef}
+              onClick={handleLoadMore}
+            >
+              Показати ще
+            </button>
+          ) : null}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TravellerStories;
